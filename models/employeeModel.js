672f22255da32745526fb33411c71e4247955784
shreedhar759 <shreedhar.ramnarain@paracon.co.za>
2025-06-11 15:10:56 +0200

    set timestamps to false for employee table

--- models/employeeModel.js
+++ models/employeeModel.js
@@ -1,37 +1,41 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../db");
 
-const Employees = sequelize.define("Employees", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  managerId: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: "Empoyees",
-      key: "id",
+const Employees = sequelize.define(
+  "Employees",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    allowNull: true,
-    defaultValue: null,
-    // autoIncrement: true,
+    managerId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "Empoyees",
+        key: "id",
+      },
+      allowNull: true,
+      defaultValue: null,
+      // autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      defaultValue: null,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      defaultValue: null,
+    },
+    departmentId: {
+      type: DataTypes.INTEGER,
+      // autoIncrement: true,
+    },
   },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: true,
-    defaultValue: null,
-  },
-  title: {
-    type: DataTypes.STRING,
-    allowNull: true,
-    defaultValue: null,
-  },
-  departmentId: {
-    type: DataTypes.INTEGER,
-    // autoIncrement: true,
-  },
-});
+  { timestamps: false }
+);
 
 console.log(Employees === sequelize.models.Employees);
 module.exports = Employees;
